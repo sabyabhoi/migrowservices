b27fdee1bf2f525c3a5be2f757b4c3028e0c3b06
sabyabhoi <sabyabhoi@gmail.com>
2022-08-24 16:12:26 +0530

    Add POST Request

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/sabyabhoi/microservices/data"
 )
@@ -21,6 +22,20 @@ func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProducts(w, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		re := regexp.MustCompile(`/([0-9]+)`)
+		group := re.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(group) != 1 {
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -33,4 +48,16 @@ func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Unable to Marshall JSON", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Post requests")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to Unmarshall JSON", http.StatusBadRequest)
+		return
+	}
+	data.AddProduct(prod)
 }
